e992e49fbe4a94f91fa741516f75d7372a5ccd14
ketema <ketemaderesa12@gmail.com>
2026-09-24 02:02:41 +0100

    Initial My Tasks Android app

---
+++ capacitor.config.js
@@ -0,0 +1,12 @@
+const config = {
+    appId: 'com.mytasks.app',
+    appName: 'My Tasks',
+    webDir: 'www',
+
+    server: {
+        url: 'http://10.130.230.155:8000',
+        cleartext: true
+    }
+};
+
+module.exports = config;
